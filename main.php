41c18378008178e45a54038cfa137782a4d74477
laurebouet <laure.bouet06@gmail.com>
2023-12-07 20:05:25 +0100

    update

--- main.php
+++ main.php
@@ -1,1 +1,14 @@
+<html lang="fr">
+<head >
+    <title>ecology</title>
+    <meta charset="UTF-8">
+</head>
+<body>
 <?php
+include("header.html");
+include("main.html");
+include("footer.html");
+
+?>
+</body>
+</html>
